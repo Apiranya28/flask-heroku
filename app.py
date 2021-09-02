5f4a4ebf9b563930f44daa03d1a8639c0ac1ae11
Apiranya28 <89176819+Apiranya28@users.noreply.github.com>
2021-09-02 11:03:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello อภิรัญญา รัตนพาหิระ เลขที่ 28 ชั้นม.4/6""
+    return "Hello อภิรัญญา รัตนพาหิระ เลขที่ 28 ชั้นม.4/6"
 
 @app.route('/api', methods=['GET'])
 def get_api():
